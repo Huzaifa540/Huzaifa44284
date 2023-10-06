4adbd5562f02a4c7db228fc9a8d4bc3a30bb68a2
Huzaifa540 <145132236+Huzaifa540@users.noreply.github.com>
2023-10-07 00:42:27 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Variables
+{
+
+
+
+    static void Main(string[] args)
+    {
+
+        // Float 
+        float float1 = 2.34f;
+        float float2 = 5.3148f;
+        float float3 = 6.18f;
+        float float4 = 35.451f;
+        float float5 = 25.18f;
+        float float6 = 35.354f;
+        float float7 = 23.18f;
+        float float8 = 132.18f;
+        float float9 = 124.618f;
+        float float10 = 2154.13342f;
+// Integer
+        int num1 = 566;
+        int num2 = 344;
+        int num3 = 134;
+        int num4 = 573;
+        int num5 = 9948;
+        int num6 = 985;
+        int num7 = 345;
+        int num8 = 3456;
+        int num9 = 1343;
+        int num10 = 2342;
+
+
+// String 
+        string stringer1 = "no";
+        string stringer2 = "why?";
+        string stringer3 = "homeless";
+        string stringer4 = "yes";
+        string stringer5 = "WHAT?";
+        string stringer6 = "i meeting";
+        string stringer7 = "i eating";
+        string stringer8 = "what is this?";
+        string stringer9 = "how?";
+        string stringer10 = "hello";
+// Boolean 
+        bool bool1 = true;
+        bool bool2 = false;
+        bool bool3 = true;
+        bool bool4 = false;
+        bool bool5 = true;
+        bool bool6 = false;
+        bool bool7 = true;
+        bool bool8 = true;
+        bool bool9 = true;
+        bool bool10 = false;
+
+    }
+}
