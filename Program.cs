e3655e60e2c92c6878dc689a5dddf75cf0f86607
Huzaifa540 <145132236+Huzaifa540@users.noreply.github.com>
2023-10-07 00:41:53 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class change
+{
+
+ 
+
+             
+    //ADD
+    public static void add(int a, int b)
+    {
+             
+        int result = a + b;
+    }
+     
+    //Subtract
+    public static void sub(int a, int b)
+    {
+ 
+        int result = a - b;
+    }
+    //DIVIDE 
+    public static void div(int a, int b)
+    {
+        
+        int result = a / b;
+    }
+    //MULTIPLY
+    public static void mul(int a, int b)
+    {
+        int result = a * b;
+    }
+    //Square
+    public static void sq(int a)
+    {
+         
+        int result = a * a;
+    }
+    //CUBE
+    public static void cube(int a)
+    {
+         
+        int result = a * a * a;
+    }
+    //METER TO KILOMETER
+    public static void m2km(int a)
+    {
+         
+        int result = a * 1000;
+    }
+    
+    //Pound to pkr
+    public static void dlr(int a)
+    {
+     
+        int result = a * 347;
+    }
+    
+    
+     
+
+
+
+
+
+
+
+
+    
+    public static void Main()
+        
+    {
+  
+
+    }
+}
